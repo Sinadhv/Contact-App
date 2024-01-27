395c7156b1fbb654678907021864c3748425e0b4
sina dehvari <cnadhv7@gmail.com>
2024-01-27 18:07:30 +0330

    create Header components of project

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./Header.module.css";
+function Header() {
+  return (
+    <div className={styles.container}>
+      <h1>Contact App</h1>
+      <p>Sina Dhv / Example Project</p>
+    </div>
+  )
+}
+
+export default Header
