39630ddfb95acdc58586ef4b2e865b3d1d2e393c
sina dehvari <cnadhv7@gmail.com>
2024-01-27 19:19:01 +0330

    create Contacts component and create form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import Contacts from "./components/Contacts";
 function App() {
   
 
   return (
     <>
      <Header />
+     <Contacts />
     </>
   );
 }
