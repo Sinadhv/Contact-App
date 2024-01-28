6833532f9b0d134804244a3cd171c1dd49169160
sina dehvari <cnadhv7@gmail.com>
2024-01-28 18:20:50 +0330

    Rendering contacts

--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 
 function ContactsList({contacts}) {
- console.log(contacts)
+
   return (
     <div>
       <h3>Contacts List</h3>
-      <ul>
-       {contacts.map (contact => <li key={contact.id}>{contact.name}</li>)}
-      </ul>
+      {contacts.length ? ( <ul>
+       {contacts.map (contact =>
+         <li key={contact.id}>
+         <p>
+           {contact.name} {contact.lastName}
+         </p>
+         <p>
+           <span>📫</span> {contact.email}
+         </p>
+         <p>
+          <span>📞</span> {contact.phone}
+         </p>
+         <button>🗑️</button>
+       </li>)}
+      </ul>) : <p>No contact yet!</p> }
+     
     </div>
   )
 }
